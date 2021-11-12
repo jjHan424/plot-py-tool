0e8008d3db391cfb8f91ebc5d1ad1160bfc3ed10
HanJunjie <HanJunjie@whu.edu.cn>
2021-11-12 19:07:17 +0800

    lalala

--- main/draw_stec.py
+++ main/draw_stec.py
@@ -1,7 +1,7 @@
 '''
 Author: HanJunjie
 Date: 2021-11-10 15:26:45
-LastEditTime: 2021-11-12 09:36:47
+LastEditTime: 2021-11-12 18:35:43
 LastEditors: Please set LastEditors
 FilePath: /plot-toolkit-master/jjHan_py_plot/main/draw_stec.py
 '''
@@ -16,8 +16,8 @@ import matplotlib.pyplot as plt
 import dataprocess as dp
 import draw as dr
 
-IPP_file1 = '/Users/hjj/Documents/HJJ/Master_1/ionofile_test/IPP2/gop60010.20IPP'
-IPP_file2 = '/Users/hjj/Documents/HJJ/Master_1/ionofile_test/IPP2/gop70010.20IPP'
+IPP_file1 = '/Users/hjj/Documents/HJJ/Master_1/ionofile_test/IPP/yarr0010.20IPP'
+IPP_file2 = '/Users/hjj/Documents/HJJ/Master_1/ionofile_test/IPP/yar30010.20IPP'
 IPP_data1 = rf.open_ipp_file(IPP_file1)
 IPP_data2 = rf.open_ipp_file(IPP_file2)
 pre_data = dp.pre_tec(IPP_data1,IPP_data2)
