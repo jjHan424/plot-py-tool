6d8018e79370a2f3d29b0132d04d2cd5465452ce
HanJunjie <HanJunjie@whu.edu.cn>
2022-04-06 10:54:41 +0800

    添加rtppp-pos画图

---
+++ main/draw_pos_rtppp.py
@@ -0,0 +1,46 @@
+'''
+Author: HanJunjie
+Date: 2021-11-29 21:26:38
+LastEditTime: 2022-04-04 15:17:57
+LastEditors: Please set LastEditors
+Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+FilePath: /plot-py-tool/main/draw_flt.py
+'''
+import os
+import sys
+sys.path.insert(0,os.path.dirname(__file__)+'/..')
+import numpy as np
+import readfile as rf
+import matplotlib as mpl
+mpl.use("TkAgg")
+import matplotlib.pyplot as plt
+import dataprocess as dp
+import draw as dr
+import seaborn as sns
+import trans as tr
+
+REF_XYZ = {"F034":[-2686347.4877,5087702.2490,2744828.4117],
+           "HKSC":[-2414267.6255,5386768.7774,2407459.7930],
+           "HKTK":[-2418093.0695,5374658.0963,2430428.9388]}
+ENU_ALL = {}
+#mode_list = ["HKLM","HKSC","HKTK"]
+mode_list = ["F034"]
+#site_list = ["HKLM","HKSC","HKTK"]
+site_list = ["F034","HKSC","HKLM"]
+filename_list = ["/Users/hjj/Documents/HJJ/Master_1/Project_MeiTuan/sixtens/Wuhan-AUG/综合改正数/终端/20220318/F034_20220318_SSRA02SIX0_K_GEC.pppar.pos",
+                 "/Users/hjj/Documents/HJJ/Master_1/IonoGrid/2021100/client/HKSC-GEC-ion.flt",
+                 "/Users/hjj/Documents/HJJ/Master_1/IonoGrid/2021100/client/HKLM-GEC-ion.flt"]
+                 
+# filename_list = [
+#                 #"/Users/hjj/Documents/HJJ/Master_1/IonoGrid/2021100/clientHKTK/5.flt",
+#                 #"/Users/hjj/Documents/HJJ/Master_1/IonoGrid/2021100/clientHKTK/6.flt",
+#                 "/Users/hjj/Documents/HJJ/Master_1/IonoGrid/2021100/clientHKTK/3.flt"
+#                  ]
+for i in range(len(mode_list)):
+    data_Raw = rf.open_flt_pos_rtpppfile(filename_list[i])
+    data_ENU = dp.XYZ2ENU_const(XYZ = data_Raw,REF_XYZ = REF_XYZ,site = site_list[i])
+    ENU_ALL[mode_list[i]] = data_ENU
+
+
+dr.plot_enu(data = ENU_ALL,type = ["E","N","U"],mode = mode_list,ylim = 1,starttime=0,begT=0,LastT=24,deltaT=2,time = "UTC",Fixed=True,delta_data = 1)
+
